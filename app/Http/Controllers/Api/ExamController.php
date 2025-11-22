6e81f8c72795a81dbeea8332e7458a7a8a0687e9
tomidev23 <tomingselingga2512@gmail.com>
2025-11-22 19:05:12 +0700

    update select tpa and cbt with api but to take the exam not succes

--- app/Http/Controllers/Api/ExamController.php
+++ app/Http/Controllers/Api/ExamController.php
@@ -4,29 +4,44 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Exam;
-use Illuminate\Http\Request;
 
 class ExamController extends Controller
 {
-   public function getTpaExams()
-{
-    // Ambil hanya ujian yang tipe TPA
-    $tpaExams = Exam::where('exam_type', 'TPA')->get();
+    // TPA
+    public function getTpaExams()
+    {
+        $exams = Exam::where('exam_type', 'tpa')->get();
 
-    // Menyesuaikan nama field agar sesuai dengan yang diharapkan Flutter
-    $formattedExams = $tpaExams->map(function ($exam) {
-        return [
-            'id' => $exam->id,
-            'title' => $exam->nama_ujian,  // Mengubah 'nama_ujian' menjadi 'title'
-            'question_count' => $exam->question_count,
-            'weight' => $exam->weight,
-            'duration' => $exam->duration,
-            'exam_type' => $exam->exam_type,
-            'logo' => $exam->logo,
-        ];
-    });
+        return response()->json(
+            $exams->map(function ($e) {
+                return [
+                    'id' => $e->id,
+                    'title' => $e->nama_ujian,
+                    'question_count' => $e->question_count,
+                    'weight' => $e->weight,
+                    'duration' => $e->duration,
+                    'logo' => asset($e->logo),
+                ];
+            })
+        );
+    }
 
-    return response()->json($formattedExams);
-}
+    // CBT
+    public function getCbtExams()
+    {
+        $exams = Exam::where('exam_type', 'cbt')->get();
 
+        return response()->json(
+            $exams->map(function ($e) {
+                return [
+                    'id' => $e->id,
+                    'title' => $e->nama_ujian,
+                    'question_count' => $e->question_count,
+                    'weight' => $e->weight,
+                    'duration' => $e->duration,
+                    'logo' => asset($e->logo),
+                ];
+            })
+        );
+    }
 }
