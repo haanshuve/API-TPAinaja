ecdd66dd6fe2a6e7d368da6b32e664e3f0420817
tomidev23 <tomingselingga2512@gmail.com>
2025-11-26 19:46:06 +0700

    update and add secure database for KBD

---
+++ app/Http/Requests/SubmitJawabanRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SubmitJawabanRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'jawaban' => 'required|string',
+            'waktu' => 'required|integer',
+        ];
+    }
+}
